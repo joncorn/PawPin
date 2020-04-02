f9504792bd89583456ec4d07d3175a46684e584b
Jon <jdcorn@live.com>
2020-04-02 14:04:29 -0600

    remove nav bar from tabbar home page

--- Zoomies/Controllers/ViewControllers/TabBar/HomePageViewController.swift
+++ Zoomies/Controllers/ViewControllers/TabBar/HomePageViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class HomePageViewController: UIViewController {
   
+  // MARK: - OUTLETS
+  
+  
+  
+  // MARK: - VIEW LIFECYCLE
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    
   }
   
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    self.navigationController?.setNavigationBarHidden(true, animated: animated)
+  }
   
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destination.
-   // Pass the selected object to the new view controller.
-   }
-   */
   
 }
